0c4db2ab41c0b2173e5b9b0ed1a1918a3f728b04
hitham <hitham.ramzy>
2021-01-17 04:30:02 +0200

    add sqlite configurations and connect the service to the attached DB

---
+++ src/main/java/com/jumai/task/entity/Customer.java
@@ -0,0 +1,21 @@
+package com.jumai.task.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String phone;
+
+}
